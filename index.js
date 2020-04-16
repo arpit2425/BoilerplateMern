3feed04c5742b51074186f0145a90b99c4c6fd40
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-04-16 15:49:41 +0530

    Routes

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const keys = require("./config/dev");
+const keys = require("./config/keys");
+const cookieparser = require("cookie-parser");
+const User = require("./model/User");
 mongoose
   .connect(keys.database, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("database connected"))
   .catch((err) => console.log(err));
+app.use(express.json());
+app.use(cookieparser());
+app.post("/api/users", async (req, res) => {
+  if (req.body) {
+    const user = await User.create(req.body);
+    return res.status(201).json({
+      status: "Success",
+      user,
+    });
+  }
+  return res.status(400).json({
+    status: "Fail",
+  });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("server started"));
